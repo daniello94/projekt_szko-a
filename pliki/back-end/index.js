b889fa6f61b72cb7156f1eef26610d10e02ab218
daniello94 <97047890+daniello94@users.noreply.github.com>
2022-07-07 15:11:55 +0200

    zadanie

--- pliki/back-end/index.js
+++ pliki/back-end/index.js
@@ -3,35 +3,35 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const http = require('http').Server(app);
-const path = require('path');
-const Message = require('./api/models/Message');
-const io = require('socket.io')(http, {
-    cors: {
-        origin: " *",
-        methods: ["GET", "POST"]
-    }
-});
-app.use(express.static(path.join(__dirname, '..', 'user', 'build')));
+// const path = require('path');
+// const Message = require('./api/models/Message');
+// const io = require('socket.io')(http, {
+//     cors: {
+//         origin: " *",
+//         methods: ["GET", "POST"]
+//     }
+// });
+// app.use(express.static(path.join(__dirname, '..', 'user', 'build')));
 
-io.on('connection', (socket) => {
-    Message.find().sort({ createAt: -1 }).limit(10).exec((err, messages) => {
-        if (err) return console.error(err)
-        socket.emit('init', messages)
-    })
+// io.on('connection', (socket) => {
+//     Message.find().sort({ createAt: -1 }).limit(10).exec((err, messages) => {
+//         if (err) return console.error(err)
+//         socket.emit('init', messages)
+//     })
 
-    socket.on('message', (msg) => {
-        const message = new Message({
-            content: msg.content,
-            name: msg.name,
-            classNr:msg.classNr
-        })
+//     socket.on('message', (msg) => {
+//         const message = new Message({
+//             content: msg.content,
+//             name: msg.name,
+//             classNr:msg.classNr
+//         })
 
-        message.save((err) => {
-            if (err) return console.error(err);
-            socket.broadcast.emit('push', msg)
-        })
-    })
-});
+//         message.save((err) => {
+//             if (err) return console.error(err);
+//             socket.broadcast.emit('push', msg)
+//         })
+//     })
+// });
 
 const userApi = require("./api/userApi")
 const chatApi = require("./api/chatApi")
